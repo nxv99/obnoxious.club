635b652ca82a1d0b717f902c6d0054c9516cb4f3
nxv99 <92235892+nxv99@users.noreply.github.com>
2022-04-03 11:10:57 +1000

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ class _app {
   backgroundToggler = false;
   shouldIgnoreVideo = false;
   effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-  brandDescription = ['11 years old kids team', 'pro roblox players', 'xane ddosers', 'trashtalkers', 'esoterik fanboys', 'hack vs hack team'];
+  brandDescription = ['programmer', 'skid', 'nev#7896', 'com', 'smm provider', 'nev // ixs'];
 
   titleChanger = (text, delay) => {
     if (!text) return;
